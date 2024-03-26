cf30d572ef5b68d4b7585225ef69a92956f22353
marvin5567 <minicreepergaming@gmail.com>
2024-03-26 22:42:04 +0200

    learnt about most if not all the core features the bot will nead

--- core.py
+++ core.py
@@ -1,5 +1,6 @@
 from interactions import Client, Intents, listen
 from interactions import slash_command, SlashContext
+from interactions import OptionType, slash_option
 
 with open('discToken.txt', 'r') as file:
     token = file.read() # token hidden using .gitignore file
@@ -14,16 +15,54 @@ bot = Client(intents=Intents.DEFAULT, token=token) # defines the bot using the t
 async def on_ready(): # when the bot is ready
     print("Bot is ready!")
 
-@listen()
-async def on_message_create(event):
-    # event is called when the bot sees a msg in a text channel
-    print(f"message received: {event.message.content}")
-
 @slash_command(
     name="meow",
     description="first command woo",
 ) # normal command
 async def meow(ctx: SlashContext):
     await ctx.send("meow") # sends 'meow' in the channel the user aks the command in
+
+@slash_command(
+    name="meo",
+    description="first command woo",
+    scopes=[guild]
+) # normal command
+async def meo(ctx: SlashContext):
+    await ctx.send("meow") # sends 'meow' in the channel the user aks the command in
+
+
+@slash_command(
+    name="num",
+    description="i like this number",
+    scopes=[guild]
+)
+@slash_option(
+    name="integer_option",
+    description="Integer Option",
+    required=True,
+    opt_type=OptionType.INTEGER
+)
+async def num(ctx: SlashContext, integer_option: int):
+    await ctx.send(f"wow i like the number: {integer_option}")
+
+@slash_command(
+    name="base",
+    description="My command base",
+    group_name="group",
+    group_description="My command group",
+    sub_cmd_name="command",
+    sub_cmd_description="My command",
+    scopes=[guild]
+)
+async def my_command_function(ctx: SlashContext):
+    await ctx.send("Hello World")
+
+@slash_command(
+    name="ping",
+    description="returns the bots ping",
+    scopes=[guild]
+)
+async def ping(ctx: SlashContext):
+    await ctx.send(bot.latency * 1000)
 
 bot.start() # starts bot
