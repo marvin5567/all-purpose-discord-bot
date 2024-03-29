c262a31c0652a6a4b1bbb396086d54096e7463b1
marvin5567 <minicreepergaming@gmail.com>
2024-03-29 22:56:12 +0200

    created FAANG stock data, along with a data parser that gives the stock data in embeds

--- cogs/stock.py
+++ cogs/stock.py
@@ -1,10 +1,47 @@
 from interactions import Extension, slash_command, SlashContext
 from interactions import Client, Intents, listen
-from interactions import Button, ButtonStyle
+from interactions import Button, ButtonStyle, ComponentContext, component_callback
+from interactions import StringSelectMenu
 from interactions import Embed
 import interactions
 import pandas
+import json
 import yfinance as yf
+
+def dataParser(data):
+
+    embed = Embed()
+    embed.set_author(name="Stock Data")
+
+    parsed = data.to_json(index=False)
+    meow = json.loads(parsed)
+
+    for key in meow['Open']:
+        open_value = meow['Open'][key]
+        print(f'Timestamp: {key}, Open Value: {open_value}')
+        embed.add_field(name="Open: ", value=str(open_value), inline=False)
+
+    for key in meow['High']:
+        open_value = meow['High'][key]
+        print(f'Timestamp: {key}, Open Value: {open_value}')
+        embed.add_field(name="High: ", value=str(open_value), inline=False)
+    
+    for key in meow['Low']:
+        open_value = meow['Low'][key]
+        print(f'Timestamp: {key}, Open Value: {open_value}')
+        embed.add_field(name="Low: ", value=str(open_value), inline=False)
+
+    for key in meow['Close']:
+        open_value = meow['Close'][key]
+        print(f'Timestamp: {key}, Open Value: {open_value}')
+        embed.add_field(name="Close: ", value=str(open_value), inline=False)
+    
+    for key in meow['Volume']:
+        open_value = meow['Volume'][key]
+        print(f'Timestamp: {key}, Open Value: {open_value}')
+        embed.add_field(name="Volume: ", value=str(open_value), inline=False)
+
+    return embed
 
 class stock(Extension):
 
@@ -34,11 +71,11 @@ class stock(Extension):
         await ctx.send(embeds=embed)
     
     @slash_command(
-        name="apple_stock",
+        name="stocks",
         description="shows you apple's stock",
         scopes=[guild]
     )
-    async def apple_stock(self, ctx: SlashContext):
+    async def stocks(self, ctx: SlashContext):
         FAANG = [
         Button(
             style=ButtonStyle.BLUE,
@@ -72,10 +109,53 @@ class stock(Extension):
         )
         ]
 
-    
+        await ctx.send("Select stock data:", components=FAANG)
+        # Microsoft, Tesla, 
+        # Cryptocurrencies
+        
+    # FAANG companies
+    @component_callback("appleStockButton")
+    async def apple(self, ctx: ComponentContext):
         # get stock data
         stock = yf.Ticker("AAPL")
+    
+        # get market data
+        data = stock.history(period='1d')
+        await ctx.send(embed=dataParser(data))
+
+    @component_callback("metaStockButton")
+    async def meta(self, ctx: ComponentContext):
+        # get stock data
+        stock = yf.Ticker("META")
         # get market data
         data = stock.history(period="1d")  
         # 5 days period, can be '1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max'
-        await ctx.send(data.to_string(index=False))
+        await ctx.send(embed=dataParser(data))
+
+    @component_callback("netflixStockButton")
+    async def netflix(self, ctx: ComponentContext):
+        # get stock data
+        stock = yf.Ticker("NFLX")
+        # get market data
+        data = stock.history(period="1d")  
+        # 5 days period, can be '1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max'
+        await ctx.send(embed=dataParser(data))
+    
+    @component_callback("amazonStockButton")
+    async def amazon(self, ctx: ComponentContext):
+        # get stock data
+        stock = yf.Ticker("AMZN")
+        # get market data
+        data = stock.history(period="1d")  
+        # 5 days period, can be '1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max'
+        await ctx.send(embed=dataParser(data))
+    
+    @component_callback("googleStockButton")
+    async def google(self, ctx: ComponentContext):
+        # get stock data
+        stock = yf.Ticker("GOOG")
+        # get market data
+        data = stock.history(period="1d")  
+        # 5 days period, can be '1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max'
+        await ctx.send(embed=dataParser(data))
+    
